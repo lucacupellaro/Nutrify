8a9195ad3bd360f8e909cb91dadabf1154800225
P1CC10N4L3X <piccionalex@gmail.com>
2023-10-17 11:38:45 +0200

    Aggiunta interfaccia GUI registrazione e login

--- src/main/java/com/example/progetto/HelloApplication.java
+++ src/main/java/com/example/progetto/HelloApplication.java
@@ -12,12 +12,13 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/aut/first-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-        stage.setTitle("Hello!");
+        stage.setTitle("Nutrify");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+
     }
 }
