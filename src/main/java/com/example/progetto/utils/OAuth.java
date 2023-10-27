670bf0b6e588fa906f659f9faef7a389ae323146
P1CC10N4L3X <piccionalex@gmail.com>
2023-10-27 15:09:01 +0200

    Risolti alcuni warning

--- src/main/java/com/example/progetto/utils/OAuth.java
+++ src/main/java/com/example/progetto/utils/OAuth.java
@@ -5,33 +5,29 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.Objects;
 import java.util.Properties;
+
 
 public class OAuth {
 
     //funzione per la creazione di una stringa casuale di lunghezza length
     public static String random64Url(int length){
-        SecureRandom rng = new SecureRandom(); //oggetto di tipo SecureRandom che mi consentirà di generare un numero di byte casuali
-        byte[] bytes = new byte[length];//realizzo un array di byte di lunghezza length
-        rng.nextBytes(bytes);//genero un array di byte casuali e li inserisco nell'array bytes
-        return random64UrlNoPadding(bytes);
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] codeVerifier = new byte[length];
+        secureRandom.nextBytes(codeVerifier);
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(codeVerifier);
     }
 
-    //funzione per eliminare tutti i caratteri non voluti in un insieme di byte
-    public static String random64UrlNoPadding(byte[] buf){
-        String base64 = new String(buf, StandardCharsets.UTF_8);
-        base64 = base64.replace("+","-");
-        base64 = base64.replace("/","_");
-        base64 = base64.replace("=","");
+    public static String generateChallenge(String codeVerifier) throws NoSuchAlgorithmException{
+        byte[] bytes = codeVerifier.getBytes(StandardCharsets.US_ASCII);
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        messageDigest.update(bytes, 0, bytes.length);
+        byte[] digest = messageDigest.digest();
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(digest);
+    }
 
-        return base64;
-    }
-    public static byte[] sha256(String buf) throws NoSuchAlgorithmException {
-        byte[] bytes = buf.getBytes();
-        MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
-        return sha256.digest(bytes);
-    }
 
     public static String generateQuery() throws NoSuchAlgorithmException, IOException{
         FileInputStream f = new FileInputStream(Objects.requireNonNull(OAuth.class.getResource("/Config/Cfg.properties")).toString().replace("file:",""));
@@ -40,12 +36,11 @@ public class OAuth {
         f.close();
         String state = OAuth.random64Url(32);
         String codeVerifier = OAuth.random64Url(32);
-        String codeChallenge = OAuth.random64UrlNoPadding(sha256(codeVerifier));
-        String reference = "none";
+        String codeChallenge = OAuth.generateChallenge(codeVerifier);
         return String.format("%s?response_type=code&scope=%s&redirect_uri=%s&client_id=%s&state=%s&code_challenge=%s&code_challenge_method=%s",
                 cfg.getProperty("authorizationEndPoint"),
                 cfg.getProperty("scope"),
-                reference,
+                cfg.getProperty("redirectUri"),
                 cfg.getProperty("clientID"),
                 state,
                 codeChallenge,
